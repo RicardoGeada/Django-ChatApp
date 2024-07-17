f87ba3cb9e879ecba29b39f391a251aca76b57f5
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-07-17 08:49:56 +0200

    added chat model

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from chat.models import Message
+
+# Gib an was im Admin Panel angezeigt wird.
+# z.B. nicht alle Felder für alle Admins anzeigen
+# evtl. Zusatzfelder einbauen (Suche nach Nutzern)
+# Filter einstellen
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'created_at', 'author', 'receiver')       # Felder für die Detailansicht (zum bearbeiten)
+    list_display = ('text', 'created_at', 'author', 'receiver') # Elemente die in der Listenansicht angezeigt werden
+    search_fields = ('text',)                                   # Erstellt Suchleiste für die Messages
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin) # Das Model wird registriert und dem Admin Panel hinzugefügt.
