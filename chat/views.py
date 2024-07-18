da154d030f5ab0f2b28c186dfa4f6416b82307e7
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-07-18 14:29:45 +0200

    added signup

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from chat.models import Chat, Message
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 @login_required(login_url='/login/')
@@ -14,13 +15,33 @@ def index(request):
     chatMessages = Message.objects.filter(chat__id=1) # Wir schauen von der Message auf das Chat Objekt mit der ID 1
     return render(request, 'chat/index.html', { 'messages' : chatMessages })
 
+
 def login_view(request):
-    redirect = request.GET.get('next')
+    redirect = request.GET.get('next') or '/chat/'
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if user:
             login(request, user)
-            return HttpResponseRedirect(request.POST.get('redirect'))
+            destination = request.POST.get('redirect') or '/chat/'
+            return HttpResponseRedirect(destination)
         else:
             return render(request, 'auth/login.html', { 'wrongPassword' : True, 'redirect' : redirect })
-    return render(request, 'auth/login.html', { 'redirect' : redirect }) 
+    return render(request, 'auth/login.html', { 'redirect' : redirect })
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        conf_password = request.POST.get('conf_password')
+        if User.objects.filter(email=email).exists():
+            return render(request, 'auth/signup.html', { 'emailExists' : True })
+        elif (password != conf_password):
+            return render(request, 'auth/signup.html', { 'passwordsDontMatch' : True })
+        else:
+            user = User.objects.create_user(name,email,password)
+            user.first_name = name
+            user.save()
+            return HttpResponseRedirect('/login/')
+    return render(request, 'auth/signup.html') 
